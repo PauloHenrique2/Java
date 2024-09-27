5d3ff0719cfe37cf74619bd37e36cfa4eb4c9851
Paulo Henrique <70448970+PauloHenrique2@users.noreply.github.com>
2024-09-27 10:37:21 -0300

    Teste do método de listagem de acessórios adicionado

--- Projeto_Carro/src/Main.java
+++ Projeto_Carro/src/Main.java
@@ -9,12 +9,17 @@ public class Main {
 
      Acessorio ac1 = new Acessorio(1, "Luz de Neon", 500.67, 2);
 
+     Acessorio ac2 = new Acessorio(2, "Escapamento silencioso", 700.54, 1);
+
      c1.Ligar();
      c1.Desligar();
      c1.Acelerar();
 
      c1.acessorios.add(ac1);
+     c1.acessorios.add(ac2);
      c1.acessorios.get(0).Ligar();
      c1.acessorios.get(0).Desligar();
+
+     c1.ListarAcessorios();
     }
-    }
+}
